385610779f8f9af5b36d633108a4d3a33722448a
Graham Gilbert <graham@grahamgilbert.com>
2016-03-18 18:33:24 +0000

    Wrong way around

--- 00-mount_s3.sh
+++ 00-mount_s3.sh
@@ -5,4 +5,4 @@ echo "$S3_IDENTITY":"$S3_CREDENTIAL" >> /etc/passwd-s3fs
 chmod 600 /etc/passwd-s3fs
 # Mount s3 bucket from environment variable
 mkdir -p /etc/puppetlabs/code
-s3fs $S3_BUCKET /etc/puppetlabs/code -o nonempty
+s3fs -o nonempty $S3_BUCKET /etc/puppetlabs/code
